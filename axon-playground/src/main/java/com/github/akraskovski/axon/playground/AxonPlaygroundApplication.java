1b5b48e7d1ee96e90f78f0faef012de3749f7cf0
akraskovski <artem.kraskovski@gmail.com>
2019-09-29 12:31:26 +0300

    initial commit

---
+++ axon-playground/src/main/java/com/github/akraskovski/axon/playground/AxonPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.github.akraskovski.axon.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AxonPlaygroundApplication.class, args);
+    }
+
+}
