84dea03908b7e9639e12b7211bf50733f75e4510
Artem Kraskovski <artem.kraskovski@gmail.com>
2019-10-01 11:35:20 +0300

    filling in the axon core api for car domain model

--- axon-playground/src/main/java/com/github/akraskovski/axon/playground/api/core/car-api.kt
+++ axon-playground/src/main/java/com/github/akraskovski/axon/playground/api/core/car-api.kt
@@ -1,2 +1,32 @@
 package com.github.akraskovski.axon.playground.api.core
 
+import org.axonframework.modelling.command.TargetAggregateIdentifier
+import java.time.Year
+import java.util.*
+
+//Commands
+data class CreateCarCommand(
+        @TargetAggregateIdentifier val carId: UUID,
+        val brand: String,
+        val model: String,
+        val year: Year
+)
+
+data class RentCarCommand(@TargetAggregateIdentifier val carId: UUID)
+data class ReturnCarCommand(@TargetAggregateIdentifier val carId: UUID)
+
+//Events
+data class CarCreatedEvent(
+        val carId: UUID,
+        val brand: String,
+        val model: String,
+        val year: Year
+)
+
+data class CarRentedEvent(val carId: UUID)
+data class CarReturnedEvent(val carId: UUID)
+
+//Queries
+class CarAvailableQuery
+
+data class CarFetchQuery(val carId: UUID)
