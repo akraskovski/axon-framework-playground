c9a1e675ec9522d4ddd4eecf0a75fa20f6a20f56
Artem Kraskovski <artem.kraskovski@gmail.com>
2019-10-01 17:35:20 +0300

    rename view model according to a domain one

--- axon-playground/src/main/java/com/github/akraskovski/axon/playground/queries/CarSummaryProjection.java
+++ axon-playground/src/main/java/com/github/akraskovski/axon/playground/queries/CarSummaryProjection.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class CarProjection {
+public class CarSummaryProjection {
 
     private final CarSummaryRepository carSummaryRepository;
 
